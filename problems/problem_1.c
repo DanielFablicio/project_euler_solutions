9b012383e5349337ecd4e741211d473e1c429660
DanielFablicio <fabliciodaniel@gmail.com>
2025-04-07 18:22:25 -0300

    Optimizes the solution to problem 1 using AP
    Switch from a loop with O(n) time complexity to an O(1) approach using
    arithmetic progression

--- problems/problem_1.c
+++ problems/problem_1.c
@@ -1,4 +1,5 @@
 //Find the sum of all the multiples of 3 or 5 below 1000
+
 #include <stdio.h>
 #include "../utils/c_ansi_textstyling.h"
 
@@ -9,10 +10,19 @@ int main() {
     const int MAX_NUM = 1000;
 
     int res = 0;
+    /* First solution [ O(n) ] Answer = 233168
     for (int i = 0; i < MAX_NUM; i++) {
         if (i % NUM_1 == 0 || i % NUM_2 == 0)
             res += i;
-        }
+        } */
+
+    // Second solution [O(1)]
+    #define AP(n1, nl, count) ((n1) + (nl)) * (count) / 2
+
+    res = AP(1, (MAX_NUM-1) / NUM_1, (MAX_NUM-1) / NUM_1) * NUM_1 +
+          AP(1, (MAX_NUM-1) / NUM_2, (MAX_NUM-1) / NUM_2) * NUM_2 -
+          AP(1, (MAX_NUM-1) / (NUM_1*NUM_2), (MAX_NUM-1) / (NUM_1*NUM_2)) * (NUM_1*NUM_2);
+
 
     printf("Answer is " FG_HI_GREEN("'%d'\n"), res);
 }
