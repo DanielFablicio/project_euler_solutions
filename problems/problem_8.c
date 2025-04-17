45c170c6c33b76b7e4d86e957da8db9eccca5158
DanielFablicio <fabliciodaniel@gmail.com>
2025-04-17 20:48:16 -0300

    Solves problem 8 - Largest Product in a Series

---
+++ problems/problem_8.c
@@ -0,0 +1,60 @@
+//Find the thirteen adjacent digits in the 1000-digit number that have
+//the greatest product. What is the value of this product?
+
+#include <stdio.h>
+#include <stdint.h>
+#include "../utils/c_ansi_textstyling.h"
+
+#define MAX_CHUNK_SIZE 20
+//Supports chunks of up to 20 digits.
+static uint64_t adjproduct(const char *strnum, size_t length,
+                           uint32_t chunk_sz)
+{
+    if (chunk_sz > MAX_CHUNK_SIZE) return 0;
+
+    uint64_t max_product = 0;
+
+    for (uint32_t i = 0; i <= (length - chunk_sz); i++) {
+        uint64_t product = 1;
+        for (uint32_t k = 0; k < chunk_sz; k++) {
+            product *= strnum[i+k] - '0';
+        }
+        if (product > max_product)
+            max_product = product;
+    }
+    return max_product;
+}
+
+
+int main(void) {
+    puts(BOLD("PROBLEM 8 - Largest Product in a Series\n"));
+
+    const char STR_NUM[] =
+        "73167176531330624919225119674426574742355349194934"
+        "96983520312774506326239578318016984801869478851843"
+        "85861560789112949495459501737958331952853208805511"
+        "12540698747158523863050715693290963295227443043557"
+        "66896648950445244523161731856403098711121722383113"
+        "62229893423380308135336276614282806444486645238749"
+        "30358907296290491560440772390713810515859307960866"
+        "70172427121883998797908792274921901699720888093776"
+        "65727333001053367881220235421809751254540594752243"
+        "52584907711670556013604839586446706324415722155397"
+        "53697817977846174064955149290862569321978468622482"
+        "83972241375657056057490261407972968652414535100474"
+        "82166370484403199890008895243450658541227588666881"
+        "16427171479924442928230863465674813919123162824586"
+        "17866458359124566529476545682848912883142607690042"
+        "24219022671055626321111109370544217506941658960408"
+        "07198403850962455444362981230987879927244284909188"
+        "84580156166097919133875499200524063689912560717606"
+        "05886116467109405077541002256983155200055935729725"
+        "71636269561882670428252483600823257530420752963450"
+    ;
+    const uint32_t CHUNCK_SZ = 13;
+
+    //Answer is '23514624000' | Av. Benchmark: < 5ms
+    uint64_t res = adjproduct(STR_NUM, sizeof(STR_NUM) - 1, CHUNCK_SZ);
+
+    printf("Answer is " FG_HI_GREEN("'%lu'\n"), res);
+}
