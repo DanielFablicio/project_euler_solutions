1f7e623abc908544f9854ff7bb5452f160397eed
DanielFablicio <fabliciodaniel@gmail.com>
2025-04-28 17:51:56 -0300

    [problem_14]: Optimizes by skipping some values
    1. Skips half the values because Collatz(2k) > (k);
    2. If `num` is odd, then pre-calculates 3*num+1 to simplify the even
       result afterwards;
    3. Save some results to be reused instead of being recalculated.

--- problems/problem_14.c
+++ problems/problem_14.c
@@ -2,6 +2,8 @@
 // chain(Collatz Sequence)?
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "../utils/c_ansi_textstyling.h"
 #include "../utils/custom_types.h"
 #include "../utils/utils.h"
@@ -23,15 +25,46 @@ int main(void) {
 
     #define UPTO 1000000
     
-    //Av. Benchmark: 995.5ms
     u32 res = 0;
     u32 max_terms = 0;
-    for (u32 n = 1; n < UPTO; n++) {
-        u32 temp = collatz(n);
+    // 1º Solution | Av. Benchmark: 331.5ms (-O1)
+    // for (u32 n = 1; n < UPTO; n++) {
+    //     u32 temp = collatz(n);
+    //     if (temp > max_terms) {
+    //         res = n;
+    //         max_terms = temp;
+    //     }
+    // }
+
+    // Av. Benchmark: 176.6ms
+    u16 *savefield = calloc(UPTO/2, sizeof(u16));
+    if (!savefield)
+        die("Error in memory allocation!");
+    for (u32 n = UPTO / 2; n < UPTO; n++) {
+        u32 temp = 0;
+        u32 num = n;
+        if (num % 2 != 0) {
+            num = 3*num+1;
+            temp++;
+        }
+        while(num % 2 == 0) {
+            num /= 2;
+            temp++;
+        }
+        bool can_save = num < UPTO/2;
+        if (can_save && savefield[num] != 0)
+            temp += savefield[num];
+        else {
+            u32 cltz = collatz(num);
+            if (can_save)
+                savefield[num] = cltz;
+            temp += cltz;
+        }
         if (temp > max_terms) {
             res = n;
             max_terms = temp;
         }
     }
     printf("Answer is " FG_HI_GREEN("'%u'\n"), res);
+    free(savefield);
 }
